5a24a7a0fd8ead8e7c4d1c4ede3a48b056531a73
Sasa Milosevic <sasmilo20@gmail.com>
2021-05-25 14:34:55 +0200

    Add birthday calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
+import { useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
 
 function App() {
   const today = new Date();
-  console.log(today);
+  // console.log(today.valueOf());
+ // console.log(today.getMonth());
 
   const dateAsString = today.toString();
-  console.log(dateAsString);
+  // console.log(dateAsString);
 
-  // const today = new Date(new Date().valueOf())
-  // .toISOString()
-  // .slice(0, 10);
+  // const [month, setMonth] = useState('');
+  // console.log(month);
 
-  // console.log(today);
+  // const [day, setDay] = useState('');
+  // console.log(day);
 
-  // const standardTime = new Date(new Date().valueOf())
-  // .toISOString()
-  // .slice(12, 16);
+  const [birthday, setBirthday] = useState('');
+  // console.log(typeof(birthday.slice(8,10)));
 
-  // console.log(standardTime);
+  // birthday is string, so we slice month from it and convert it to number,
+  // than subtract 1 because JavaScript calculates months from 0 to 11,
+  // convert it back to string and define it should have two characters,
+  // and if there is only one character, it should add zero in front of it
+  const monthOfBirth = String(Number(birthday.slice(5,7)) - 1).padStart(2, '0');
+  // console.log(monthOfBirth);
+
+  const birthdayForCalc = new Date(birthday.slice(0,4), monthOfBirth,  birthday.slice(8,10));
+  // console.log(birthdayForCalc);
+  // console.log(today.getMonth());
+  // console.log(today.getFullYear());
+  // console.log(birthdayForCalc.getMonth());
+
+  let nextBirthday;
+
+
+  if (today.getMonth() > birthdayForCalc.getMonth() || (today.getMonth() === birthdayForCalc.getMonth() && today.getDate() > birthdayForCalc.getDate())) {
+    const nextBirthdayYear = String(today.getFullYear() + 1);
+      nextBirthday = new Date(nextBirthdayYear, monthOfBirth,  birthday.slice(8,10));
+      // console.log(nextBirthday);
+
+  }
+  else {
+    const nextBirthdayYear = String(today.getFullYear());
+      nextBirthday = new Date(nextBirthdayYear, monthOfBirth,  birthday.slice(8,10));
+      // console.log(nextBirthday);
+
+  }
+
+  const daysTillBirthday = Math.ceil((nextBirthday.valueOf() - today.valueOf())/86400000);
+  const howOld = Math.floor((nextBirthday.valueOf() - birthdayForCalc.valueOf())/(365*86400000));
+  console.log(howOld);
+
+
 
   return (
     <div className="App">
@@ -26,8 +60,18 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Today is {dateAsString.slice(0,21)}</p>
 
-        <span>Enter your birthday:</span>
-        <input type="date" />
+        Enter your date of birth:
+        <input type="date"
+        onChange={(event) => {
+          const newBday = event.target.value;
+          setBirthday(newBday);
+}}/>
+
+
+        <p> Your next birthday is on {nextBirthday.toString().slice(0,16)}.</p>
+        <p>There is {daysTillBirthday } days until your birthday.</p>
+        <p>On that day you will be {howOld} years old.</p>
+
       </header>
     </div>
   );
